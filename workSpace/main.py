18a4f78037c523a2660ba4344e9e4eede49e0ca5
Heather_sumac <hmore@sfu.ca>
2023-08-25 18:13:14 +0200

    Use functions for printing; format output
    Makes it easier to read what's happening.

--- workSpace/main.py
+++ workSpace/main.py
@@ -16,22 +16,22 @@ from sysfont import sysfont
 # 1420 = tap water
 # 2400 = very dry shamrock
 # 3600 = fully dry
-watering_threshold = [1700,
-                       1650,
-                       1600
+watering_threshold = [2850, # ygb
+                       2400, # bro
+                       2200 # kkk
                        ]
 
 # Watering time (s)
-watering_time = [1,
-                  1,
-                  1
+watering_time = [5, # ygb
+                  10, # bro
+                  15 # kkk
                   ]
 
 # Time between waterings (s)
-watering_interval = 60#3600
+watering_interval = 2*3600
 
 # Time between measurements (s)
-measuring_interval = 30
+measuring_interval = 0.25*3600
 
 # Define input pins
 #button_interrupt = Pin(23, Pin.IN)
@@ -95,37 +95,37 @@ screen_size = str(tft.size())
 
 
 ## DEFINE FUNCTIONS #######################
+
+def get_status_as_string():
+  t = time.localtime()
+  out_string = ("Measured @" + " day=" + str(t[2]) + " time=" + str(t[3]) + ":" + str(t[4])
+  + "\nygb=" + str(sensor_ygb.read())
+  + "\nbro=" + str(sensor_bro.read())
+  + "\nkkk=" + str(sensor_kkk.read())
+  + "\npumps=" + str(str(pump_ygb.value()))
+  + " " + str(pump_bro.value())
+  + " " + str(pump_kkk.value()))
+  return out_string
+  
 def print_status_to_lcd():
   tft.fill(TFT.BLACK)
   tft.text(aPos=(0, 0), 
-          aString="Measured @ " + str(time.localtime())
-          + " ygb=" + str(sensor_ygb.read())
-          + " bro=" + str(sensor_bro.read())
-          + " kkk=" + str(sensor_kkk.read())
-          + " pumps=" + str(str(pump_ygb.value()))
-          + " " + str(pump_bro.value())
-          + " " + str(pump_kkk.value()),
+          aString=get_status_as_string(),
           aColor=TFT.WHITE, 
           aFont=sysfont, 
           aSize=1,
           nowrap=False
           )
   time.sleep_ms(1)
+  
+def print_status_to_terminal():
+  print(get_status_as_string())
 
 
 ## CHECK VALUES ###########################
 
-# Print pin values and time
-print("Started at:  " + str(time.localtime()))
-#print("button_interrupt = " + str(button_interrupt.value()))
-print("sensor_ygb = " + str(sensor_ygb.read()))
-print("sensor_bro = " + str(sensor_bro.read()))
-print("sensor_kkk = " + str(sensor_kkk.read()))
-print("pump_ygb = " + str(pump_ygb.value()))
-print("pump_bro = " + str(pump_bro.value()))
-print("pump_kkk = " + str(pump_kkk.value()))
-
-# Print pin values to LCD
+# Print status
+print_status_to_terminal()
 print_status_to_lcd()
 
 
@@ -173,7 +173,6 @@ while True:
     sensorVal[ind] = isensor.read()
     if isensor.read() > watering_threshold[ind]:
       # Keep track of sensor and pump information
-      #print("pump # " + str(ind))
       isPumpOn[ind] = 1      
       # Turn on water for prescribed time
       pumps[ind].value(1)
@@ -186,14 +185,8 @@ while True:
   # https://github.com/artem-smotrakov/esp32-weather-google-sheets
   #print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
   
-  #print("sensor_ygb = " + str(sensor_ygb.read()))
-  #print("sensor_bro = " + str(sensor_bro.read()))
-  
-  print("Measured at:  " + str(time.localtime()))
-  print("Sensor values: " + (' '.join(str(x) for x in sensorVal)))
-  print("Pump status: " + (' '.join(str(x) for x in isPumpOn)) + "\n")
-  
-  # Print pin values to LCD
+  # Print status
+  print_status_to_terminal()
   print_status_to_lcd()
   
   #time.sleep(watering_interval)
@@ -205,15 +198,16 @@ while True:
     for ind, isensor in enumerate(sensors):
       sensorVal[ind] = isensor.read()
       
-    print("Measured at:  " + str(time.localtime()))
-    print("Sensor values: " + (' '.join(str(x) for x in sensorVal)))
-    
-    # Print pin values to LCD
+    # Print status
+    print_status_to_terminal()
     print_status_to_lcd()
     
     time.sleep(measuring_interval)
     
  
 print("Exited")
+
+
+
 
 
